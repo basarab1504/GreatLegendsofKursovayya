7e85aa2f9a6059141467ac43bc5d181ed810cce7
basarab1504 <45500276+basarab1504@users.noreply.github.com>
2020-03-19 00:57:03 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,39 +1,36 @@
 const express = require("express");
-const app = express();
 const fs = require('fs');
+const app = express();
 
 app.set('view engine', 'pug');
 
 app.get('/', function (req, res) {
-    res.render('index', { courses: getCourses()});
+    res.render('index', { courses: getCourses() });
 });
 
 app.get('/course', function (req, res, next) {
-    res.render('course', { course: req.query.sel_course, subjects: getSubjects(req.query.sel_course)});
+    res.render('course', { course: req.query.sel_course, subjects: getSubjects(req.query.sel_course) });
 });
 
 app.get('/calculate', function (req, res) {
-    res.render('report', { course: req.query.sel_course, subjects: getResult(req.query)});
+    res.render('report', { course: req.query.sel_course, subjects: getResult(req.query) });
 });
 
 app.listen(3000);
 
-function getCourses()
-{
+function getCourses() {
     return fs.readdirSync('./files');
 }
 
-function getSubjects(course)
-{
+function getSubjects(course) {
     return JSON.parse(fs.readFileSync('./files/' + course, 'utf8'));
 }
 
-function getResult(query)
-{
+function getResult(query) {
     let results = [];
 
     getSubjects(query.sel_course).forEach(element => {
-        results.push({"subject": element.subject, "result": element.hours - query[element.subject] <= 0 ? "Прошел" : "Не прошел"});
+        results.push({ "subject": element.subject, "result": element.hours - query[element.subject] <= 0 ? "Прошел" : "Не прошел" });
     });
 
     return results;
